6bd048024a9cec60c9f5c179d03ae58211d9ac76
Coding-Chobo <dbdaldrb1@gmail.com>
2024-10-03 01:09:36 +0900

    1 - 7 완료

--- Practical/Practical 1/Practical 1/Practical 1 - 7.cpp
+++ Practical/Practical 1/Practical 1/Practical 1 - 7.cpp
@@ -1,19 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <gl/glew.h> //--- 필요한 헤더파일 include
+#include <gl/glew.h> // 필요한 헤더파일 include
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
 #include <ctime>
 #include <cstdlib> // rand() 함수를 사용하기 위해 추가
 #include <stdio.h>
-GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
-GLuint vertexShader, fragmentShader; //--- 세이더 객체
-GLuint shaderProgramID; //--- 셰이더 프로그램
+#include <iomanip> // For debug output precision
 
+#define Max 10
+#define Max_vertex 40
+#define ROW 1000
+#define COL 1000
 
-GLuint vao, vbo[2];
+GLchar* vertexSource, * fragmentSource; // 소스코드 저장 변수
+GLuint vertexShader, fragmentShader; // 세이더 객체
+GLuint shaderProgramID; // 셰이더 프로그램
+GLuint vao, vbo[2]; // VAO와 VBO
+
+//----------------------------- 구조체 선언 -------------------------------
+
+//--------------------------- 전역 변수 ---------------------------------
+float xpos{}, ypos{}; // 마우스 클릭 위치 좌표
+bool canmake{}; // 도형을 생성할 수 있는지 여부
+int currentblockcnt{}; // 현재 생성된 도형 개수
+float radius{ 0.15f }; // 도형의 반지름
+GLfloat colors[Max][3][3]; // 각 도형의 색상
+GLfloat vertex[Max_vertex][3];
+int shape[Max];
+int currentvertexcnt{};
 //----------------------------- 함수 선언 -------------------------------------
-
+void init_color();
 void make_vertexShaders();
 void make_fragmentShaders();
 GLuint make_shaderProgram();
@@ -22,156 +39,416 @@ GLvoid Reshape(int w, int h);
 GLint width, height;
 void InitBuffer();
 char* filetobuf(const char* file);
-
+GLvoid Keyboard(unsigned char key, int x, int y);
+void Timer(int value); // 타이머 콜백 함수
+void Mouse(int button, int state, int x, int y);
+void Motion(int x, int y);
+void MakeSquare(int point);
+void UpdateVBO();
+void PrintFigureDebugInfo();
 //---------------------------------------------------------------------
-const GLfloat triShape[3][3] = { //--- 삼각형 위치 값
-{ -0.5, -0.5, 0.0 }, { 0.5, -0.5, 0.0 }, { 0.0, 0.5, 0.0} };
-const GLfloat colors[3][3] = { //--- 삼각형 꼭지점 색상
-{ 1.0, 0.0, 0.0 }, { 0.0, 1.0, 0.0 }, { 0.0, 0.0, 1.0 } };
 
-void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
-{
-	//--- 윈도우 생성하기
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("Example1");
-	//--- GLEW 초기화하기
-	glewExperimental = GL_TRUE;
-	glewInit();
+// 색상 초기화 함수
+void init_color() {
+    GLfloat presetColors[3][3] = {
+        {1.0f, 0.0f, 0.0f},  // Red
+        {0.0f, 1.0f, 0.0f},  // Green
+        {0.0f, 0.0f, 1.0f}   // Blue
+    };
 
-	InitBuffer();
-	make_vertexShaders();
-	make_fragmentShaders();
-	shaderProgramID = make_shaderProgram();
-	glutDisplayFunc(drawScene);
-	glutReshapeFunc(Reshape);
-	glutMainLoop();
+    // 각 도형의 색상 값을 초기화
+    for (int i = 0; i < Max; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            colors[i][j][0] = presetColors[i % 3][0]; // R value
+            colors[i][j][1] = presetColors[i % 3][1]; // G value
+            colors[i][j][2] = presetColors[i % 3][2]; // B value
+        }
+    }
 }
 
-GLvoid drawScene()
-{
-	GLclampf rColor{ 0.5f }, gColor{ 0.5f }, bColor{0.5f};
-	//--- 변경된 배경색 설정
-	glClearColor(rColor, gColor, bColor, 1.0f);
-	//glClearColor(1.0, 1.0, 1.0, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//--- 렌더링 파이프라인에 세이더 불러오기
-	glUseProgram(shaderProgramID);
-	//--- 사용할 VAO 불러오기
-	glBindVertexArray(vao);
-	//--- 삼각형 그리기
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glutSwapBuffers(); //--- 화면에 출력하기
-}
-//--- 다시그리기 콜백 함수
-GLvoid Reshape(int w, int h)
-{
-	glViewport(0, 0, w, h);
-}
+// 프로그램의 메인 함수
+void main(int argc, char** argv) {
+    // 윈도우 생성
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(ROW, COL);
+    glutCreateWindow("Practical 1");
 
+    // GLEW 초기화
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-void InitBuffer()
-{
-	glGenVertexArrays(1, &vao); //--- VAO 를 지정하고 할당하기
-	glBindVertexArray(vao); //--- VAO를 바인드하기
-	glGenBuffers(2, vbo); //--- 2개의 VBO를 지정하고 할당하기
-	//--- 1번째 VBO를 활성화하여 바인드하고, 버텍스 속성 (좌표값)을 저장
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	//--- 변수 diamond 에서 버텍스 데이터 값을 버퍼에 복사한다.
-	//--- triShape 배열의 사이즈: 9 * float
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), triShape, GL_STATIC_DRAW);
-	//--- 좌표값을 attribute 인덱스 0번에 명시한다: 버텍스 당 3* float
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	//--- attribute 인덱스 0번을 사용가능하게 함
-	glEnableVertexAttribArray(0);
-	//--- 2번째 VBO를 활성화 하여 바인드 하고, 버텍스 속성 (색상)을 저장
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	//--- 변수 colors에서 버텍스 색상을 복사한다.
-	//--- colors 배열의 사이즈: 9 *float
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), colors, GL_STATIC_DRAW);
-	//--- 색상값을 attribute 인덱스 1번에 명시한다: 버텍스 당 3*float
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	//--- attribute 인덱스 1번을 사용 가능하게 함.
-	glEnableVertexAttribArray(1);
+    init_color(); // 색상 초기화
+    InitBuffer(); // 버퍼 초기화
+    make_vertexShaders(); // 버텍스 셰이더 생성
+    make_fragmentShaders(); // 프래그먼트 셰이더 생성
+    shaderProgramID = make_shaderProgram(); // 셰이더 프로그램 생성
+
+    // 콜백 함수 등록
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(Reshape);
+    glutMouseFunc(Mouse);
+    glutKeyboardFunc(Keyboard);
+
+    glutMainLoop(); // 메인 루프 진입
 }
 
+// 도형을 생성하고 정점 정보를 설정하는 함수
+void MakeSquare(int point) {
+    if (currentblockcnt >= Max) return; // 최대 도형 개수를 초과하지 않도록 함
+
+    shape[currentblockcnt]= point;
+    switch (point) {
+    case 1: // 작은 사각형(점)
+        vertex[currentvertexcnt][0] = xpos - radius / 3;
+        vertex[currentvertexcnt][1] = ypos - radius / 3;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos - radius / 3;
+        vertex[currentvertexcnt][1] = ypos + radius / 3;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos + radius / 3;
+        vertex[currentvertexcnt][1] = ypos + radius / 3;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos + radius / 3;
+        vertex[currentvertexcnt][1] = ypos - radius / 3;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        break;
+
+    case 2: // 선
+        vertex[currentvertexcnt][0] = xpos - radius;
+        vertex[currentvertexcnt][1] = ypos - radius;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos + radius;
+        vertex[currentvertexcnt][1] = ypos + radius;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        break;
+
+    case 3: // 삼각형
+        vertex[currentvertexcnt][0] = xpos;
+        vertex[currentvertexcnt][1] = ypos + radius;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos - radius * 0.85;
+        vertex[currentvertexcnt][1] = ypos - radius / 2;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos + radius * 0.85;
+        vertex[currentvertexcnt][1] = ypos - radius / 2;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        break;
+
+    case 4: // 사각형
+        vertex[currentvertexcnt][0] = xpos - radius;
+        vertex[currentvertexcnt][1] = ypos - radius;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos - radius;
+        vertex[currentvertexcnt][1] = ypos + radius;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos + radius;
+        vertex[currentvertexcnt][1] = ypos + radius;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        vertex[currentvertexcnt][0] = xpos + radius;
+        vertex[currentvertexcnt][1] = ypos - radius;
+        vertex[currentvertexcnt][2] = 0.0f;
+        currentvertexcnt++;
+        break;
+
+    default:
+        break;
+    }
+    
+    currentblockcnt++; // 도형 개수 증가
+
+    // VBO 데이터를 업데이트하여 GPU에 다시 전송
+    UpdateVBO();
+
+    // 화면을 다시 그리도록 요청
+    glutPostRedisplay();
+}
+
+// VAO 및 VBO 초기화
+void InitBuffer() {
+    glGenVertexArrays(1, &vao); // VAO 생성
+    glBindVertexArray(vao); // VAO 바인딩
+
+    glGenBuffers(2, vbo); // 2개의 VBO 생성
+
+    // 정점 좌표 데이터를 VBO에 복사
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_DYNAMIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    // 색상 데이터를 VBO에 복사
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_DYNAMIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(1);
+}
+
+// VBO 데이터를 업데이트하는 함수
+void UpdateVBO() {
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertex), vertex); // GPU에 새로운 정점 데이터 전송
+}
+
+// 화면 그리기 콜백 함수
+GLvoid drawScene() {
+    // 배경색 설정
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(shaderProgramID); // 셰이더 프로그램 사용
+
+    glBindVertexArray(vao); // VAO 바인딩
+
+    // 도형 그리기: 각 도형의 정점 개수에 따라 다르게 그려야 함
+    int offset = 0;
+    for (int i = 0; i < currentblockcnt; ++i) {
+        if (shape[i] == 4) {
+            glDrawArrays(GL_TRIANGLE_FAN, offset, 4); // 사각형: 4개의 정점
+            offset += 4;
+        }
+        else if (shape[i] == 2) {
+            glDrawArrays(GL_LINES, offset, 2); // 선: 2개의 정점
+            offset += 2;
+        }
+        else if (shape[i] == 3) {
+            glDrawArrays(GL_TRIANGLES, offset, 3); // 삼각형: 3개의 정점
+            offset += 3;
+        }
+        else if (shape[i] == 1) {
+            glDrawArrays(GL_TRIANGLE_FAN, offset, 4); // 작은 사각형: 4개의 정점
+            offset += 4;
+        }
+    }
+
+    glutSwapBuffers(); // 화면에 출력
+}
+
+// 윈도우 리사이즈 콜백 함수
+GLvoid Reshape(int w, int h) {
+    glViewport(0, 0, w, h);
+}
+
+// 마우스 클릭 이벤트 콜백 함수
+void Mouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        canmake = true;
+        xpos = (static_cast<float>(x) / (ROW / 2.0f)) - 1.0f; // OpenGL 좌표로 변환
+        ypos = 1.0f - (static_cast<float>(y) / (COL / 2.0f)); // OpenGL 좌표로 변환
+    }
+    else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+        canmake = false;
+    }
+}
+
+// 키보드 입력 이벤트 콜백 함수
+GLvoid Keyboard(unsigned char key, int x, int y) {
+    if (canmake) {
+        switch (key) {
+        case 'p': MakeSquare(1); break; // 작은 사각형(점) 생성
+        case 'l': MakeSquare(2); break; // 선 생성
+        case 't': MakeSquare(3); break; // 삼각형 생성
+        case 'r': MakeSquare(4); break; // 사각형 생성
+        }
+    }
+    int edited_vertex{};
+    int select_figure{};
+    if (currentblockcnt > 0)
+    {
+        switch (key)
+        {
+        case 'w':
+            edited_vertex = 0;
+            select_figure = rand() % currentblockcnt;  // 바뀌는 도형 인덱스
+            for (int i = 0; i < select_figure; i++)
+            {
+                if (shape[i] == 1)
+                {
+                    edited_vertex += shape[i] * 4;
+                }
+                else
+                {
+                    edited_vertex += shape[i];
+                }
+
+            }
+            if (shape[select_figure] == 1)
+            {
+                for (int i = 0; i < 4; i++) // 바뀌는 도형의 버텍스 수
+                {
+                    vertex[edited_vertex + i][1] += radius;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < shape[select_figure]; i++) // 바뀌는 도형의 버텍스 수
+                {
+                    vertex[edited_vertex + i][1] += radius;
+                }
+            }
+            break;
+        case 'a':
+            edited_vertex = 0;
+            select_figure = rand() % currentblockcnt;  // 바뀌는 도형 인덱스
+            for (int i = 0; i < select_figure; i++)
+            {
+                if (shape[i] == 1)
+                {
+                    edited_vertex += shape[i] * 4;
+                }
+                else
+                {
+                    edited_vertex += shape[i];
+                }
+            }
+            if (shape[select_figure] == 1)
+            {
+                for (int i = 0; i < 4; i++) // 바뀌는 도형의 버텍스 수
+                {
+                    vertex[edited_vertex + i][0] -= radius;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < shape[select_figure]; i++) // 바뀌는 도형의 버텍스 수
+                {
+                    vertex[edited_vertex + i][0] -= radius;
+                }
+            }
+            break;
+        case 's':
+            edited_vertex = 0;
+            select_figure = rand() % currentblockcnt;  // 바뀌는 도형 인덱스
+            for (int i = 0; i < select_figure; i++)
+            {
+                if (shape[i] == 1)
+                {
+                    edited_vertex += shape[i] * 4;
+                }
+                else
+                {
+                    edited_vertex += shape[i];
+                }
+            }
+            if (shape[select_figure] == 1)
+            {
+                for (int i = 0; i < 4; i++) // 바뀌는 도형의 버텍스 수
+                {
+                    vertex[edited_vertex + i][1] -= radius;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < shape[select_figure]; i++) // 바뀌는 도형의 버텍스 수
+                {
+                    vertex[edited_vertex + i][1] -= radius;
+                }
+            }
+            break;
+        case 'd': 
+            edited_vertex = 0;
+            select_figure = rand() % currentblockcnt;  // 바뀌는 도형 인덱스
+            for (int i = 0; i < select_figure; i++)
+            {
+                if (shape[i] == 1)
+                {
+                    edited_vertex += shape[i] * 4;
+                }
+                else
+                {
+                    edited_vertex += shape[i];
+                }
+            }
+            if (shape[select_figure] == 1)
+            {
+                for (int i = 0; i < 4; i++) // 바뀌는 도형의 버텍스 수
+                {
+                    vertex[edited_vertex + i][0] += radius;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < shape[select_figure]; i++) // 바뀌는 도형의 버텍스 수
+                {
+                    vertex[edited_vertex + i][0] += radius;
+                }
+            }
+
+            break;
+        case 'c':
+            currentblockcnt = 0;
+            currentvertexcnt = 0;
+            ZeroMemory(vertex, 100);
+
+        default:
+            break;
+
+        }
+    }
+    
+    // VBO 데이터를 업데이트하여 GPU에 다시 전송
+    UpdateVBO();
+
+    // 화면을 다시 그리도록 요청
+    glutPostRedisplay();
+}
+
+// 셰이더 프로그램 생성 함수
 GLuint make_shaderProgram() {
-	GLuint shaderID = glCreateProgram(); //--- 세이더 프로그램 만들기
-	GLint result;
-	GLchar errorLog[512];
-	glAttachShader(shaderID, vertexShader); //--- 세이더 프로그램에 버텍스 세이더 붙이기
-	glAttachShader(shaderID, fragmentShader); //--- 세이더 프로그램에 프래그먼트 세이더 붙이기
-	glLinkProgram(shaderID); //--- 세이더 프로그램 링크하기
-	glDeleteShader(vertexShader); //--- 세이더 프로그램에 링크하여 세이더 객체 자체는 삭제 가능
-	glDeleteShader(fragmentShader);
-	glGetProgramiv(shaderID, GL_LINK_STATUS, &result); // ---세이더가 잘 연결되었는지 체크하기
-	if (!result) {
-		glGetProgramInfoLog(shaderID, 512, NULL, errorLog);
-		std::cerr << "ERROR: shader program 연결 실패\n" << errorLog << std::endl;
-		return false;
-	}
-	return shaderID;
+    GLuint shaderID = glCreateProgram();
+    glAttachShader(shaderID, vertexShader);
+    glAttachShader(shaderID, fragmentShader);
+    glLinkProgram(shaderID);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return shaderID;
 }
 
-void make_vertexShaders()
-{
-	vertexSource = filetobuf("vertex.vs");
-		//--- 버텍스 세이더 객체 만들기
-		vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	//--- 세이더 코드를 세이더 객체에 넣기
-	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
-	//--- 버텍스 세이더 컴파일하기
-	glCompileShader(vertexShader);
-	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
-	GLint result;
-	GLchar errorLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
-	if(!result)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
-		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
-		return;
-	}
-}
-char* filetobuf(const char* file)
-{
-	FILE* fptr;
-	long length;
-	char* buf;
-	fptr = fopen(file, "rb"); // Open file for reading 
-	if (!fptr) // Return NULL on failure 
-		return NULL;
-	fseek(fptr, 0, SEEK_END); // Seek to the end of the file 
-	length = ftell(fptr); // Find out how many bytes into the file we are 
-	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator 
-	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file 
-	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer 
-	fclose(fptr); // Close the file 
-	buf[length] = 0; // Null terminator 
-	return buf; // Return the buffer 
+// 버텍스 셰이더 생성 함수
+void make_vertexShaders() {
+    vertexSource = filetobuf("vertex.vs");
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+    glCompileShader(vertexShader);
 }
 
+// 프래그먼트 셰이더 생성 함수
+void make_fragmentShaders() {
+    fragmentSource = filetobuf("fragment.fs");
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+    glCompileShader(fragmentShader);
+}
 
-//--- 프래그먼트 세이더 객체 만들기
-void make_fragmentShaders()
-{
-	fragmentSource = filetobuf("fragment.fs");
-	//--- 프래그먼트 세이더 객체 만들기
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	//--- 세이더 코드를 세이더 객체에 넣기
-	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
-	//--- 프래그먼트 세이더 컴파일
-	glCompileShader(fragmentShader);
-	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
-	GLint result;
-	GLchar errorLog[512];
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
-		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
-		return;
-	}
+// 파일을 버퍼로 읽어오는 함수
+char* filetobuf(const char* file) {
+    FILE* fptr;
+    long length;
+    char* buf;
+
+    fptr = fopen(file, "rb");
+    if (!fptr) return NULL;
+
+    fseek(fptr, 0, SEEK_END);
+    length = ftell(fptr);
+    buf = (char*)malloc(length + 1);
+    fseek(fptr, 0, SEEK_SET);
+    fread(buf, length, 1, fptr);
+    fclose(fptr);
+
+    buf[length] = 0;
+    return buf;
 }
